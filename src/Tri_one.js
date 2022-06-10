19509bbefd41579bedcf34dab1227658a33d0600
balaji <105437098+Balajikumar10@users.noreply.github.com>
2022-06-10 18:24:38 +0530

    aa

--- src/Tri_one.js
+++ src/Tri_one.js
@@ -4,9 +4,7 @@ function Tri_one() {
     return (
         <div>
             Tri_one
-            <h1>first_one</h1>
-            <h2>second</h2>
-            <h3>third</h3>
+           
         </div>
 
     )

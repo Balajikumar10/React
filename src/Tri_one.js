f68e37daaf079e9f1987d0b4f20cc63d55377a72
BalajiKumar10 <balajikumar@valorpaytech.com>
2022-06-10 17:10:46 +0530

    first_attempt

---
+++ src/Tri_one.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tri_one() {
+    return (
+        <div>
+            Tri_one
+            <h1>first_one</h1>
+        </div>
+
+    )
+}
+export default Tri_one
